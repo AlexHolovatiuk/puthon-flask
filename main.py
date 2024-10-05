867102b9b0b2222f188fa2ee8e3e3c5ebf7bccbf
AlexHol <alexhol.the.best@gmail.com>
2024-10-05 12:20:52 +0300

    lesson 25

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
+from codecs import replace_errors
+from crypt import methods
+from email.policy import default
+from flask import request
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    role = db.Column(db.Integer(), default=0, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+class Post(db.Model):
+    __tablename__ = 'Posts'
+    id = db.Column(db.Integer, primary_key=True)
+    post_name = db.Column(db.String(255), nullable=False)
+    post_text = db.Column(db.Text(), nullable=False)
+    post_image = db.Column(db.String(255), nullable=False)
+    continent = db.Column(db.String(255), nullable=False)
+    created_on = db.Column(db.Date(), default=datetime.utcnow())
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
-    # return "<h1>Моя сторінка на Flask 1</h1>"
     return render_template('index.html')
 
 
@@ -16,19 +52,40 @@ def login():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html',title='About')
 
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html')
+    new_articles = ['How to avoid expensive travel mistakes', 'Top 5 places to experience supernatural forces',
+                    'Three wonderfully bizarre Mexican festivals', 'The 20 greenest destinations on Earth',
+                    'How to survive on a desert island']
+    return render_template('articles.html', articles=new_articles)
 
+@app.route('/add_article')
+def add_article():
+    return render_template('add_article.html')
 
 @app.route('/details')
 def details():
     return render_template('details.html')
 
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
 
-# Лише для локального сервера
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template('index.html')
+    else:
+        return render_template('add_user.html')
+
+
+# Лише для локаотного сервера (закоментувати)
 if __name__ == '__main__':
     app.run(debug=True)
